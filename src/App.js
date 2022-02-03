68274a5b896a2221d7a443bdd2365834c07a27b4
EthanHermsey <ivanhermse@gmail.com>
2022-02-03 01:41:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ class App extends Component {
                 <Panel {...item} key={index} />
             )
         })
-
 		return (		 
 			<div className="App bg-main">
-			<div className="container-wide mt-5">
-            	<div className="row gy-4">
-					{ itemPanels }
-                </div>
+				<div className="container-wide mt-5">
+                    <div className="row gy-4">
+					    { itemPanels }
+                    </div>
+				</div>
+                <footer>
+                    <a href="mailto:ethan@basis64.nl" target="_blank" rel="noreferrer">
+                        <div id="email" className="bg-button text-black text-end pe-1">ethan@basis64.nl</div>
+                    </a>
+                    <a href="https://github.com/EthanHermsey?tab=repositories" target="_blank" rel="noreferrer">
+                        <div id="git" className="bg-button text-black text-end pe-1">EthanHermsey</div>
+                    </a>
+                </footer>
 			</div>
-            <footer>
-				<a href="mailto:ethan@basis64.nl" target="_blank" rel="noreferrer">
-					<div id="email" className="bg-button text-black text-end pe-1">ethan@basis64.nl</div>
-				</a>
-				<a href="https://github.com/EthanHermsey?tab=repositories" target="_blank" rel="noreferrer">
-					<div id="git" className="bg-button text-black text-end pe-1">EthanHermsey</div>
-				</a>
-            </footer>			
-		);
+		);		
 	}
 };
 
